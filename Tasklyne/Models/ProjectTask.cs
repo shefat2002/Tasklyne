936a68e644b699e4a2d9b9b104911679f96a2457
shefat2002 <shefat2002@gmail.com>
2026-01-05 19:32:01 +0600

    updated

---
+++ Tasklyne/Models/ProjectTask.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Tasklyne.Models;
+
+public class ProjectTask
+{
+    public int Id { get; set; }
+    [Required]
+    [StringLength(20, ErrorMessage = "Name cannot be longer than 20 characters.")]
+    public string Name { get; set; } = string.Empty;
+    [StringLength(30, ErrorMessage = "Description cannot be longer than 30 characters.")]
+    public string? Description { get; set; }
+    public DateTime CreatedAt { get; set; } = DateTime.Now;
+    public DateTime? DueDate { get; set; } = DateTime.Now.AddDays(7);
+    [ForeignKey("Creator")]
+    public string CreatedById { get; set; } = string.Empty;
+    
+    [ForeignKey("Project")]
+    public int? ProjectId{ get; set; }
+
+
+
+    // Navigation Properties
+    [ValidateNever]
+    public Project? Project { get; set; }
+    [ValidateNever]
+    public Employee? CreatedBy { get; set; }
+    public ICollection<AssignedTask>? AssignedTasks { get; set; } = new List<AssignedTask>();
+
+
+
+}
---
+++ Tasklyne/Views/Shared/_navbar.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Tasklyne.Views.Shared
+{
+    public class _navbarModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
