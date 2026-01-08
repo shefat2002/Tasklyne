31b21da4b29de8e727eaa645ac83a77145c01e5e
shefat2002 <shefat2002@gmail.com>
2026-01-08 19:37:49 +0600

    deleted adminlte files

---
+++ Tasklyne/Controllers/DepartmentController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Tasklyne.Data;
+
+namespace Tasklyne.Controllers;
+
+public class DepartmentController : Controller
+{
+    private readonly ILogger<DepartmentController> _logger;
+    private readonly ApplicationDbContext _context;
+    
+    public DepartmentController(ILogger<DepartmentController> logger, ApplicationDbContext context)
+    {
+        _logger = logger;
+        _context = context;
+    }
+
+    public IActionResult Index()
+    {
+        var departments = _context.Departments.ToList();
+        ViewData["Departments"] = departments;
+        return View();
+    }
+    
+    
+}
