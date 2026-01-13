0654145b00c5d1fc9eb707573734062dc13e10c2
shefat2002 <shefat2002@gmail.com>
2026-01-14 00:34:43 +0600

    added generic interface

--- Tasklyne.Application/RepositoryInterfaces/ITasklyne.cs
+++ Tasklyne.Application/RepositoryInterfaces/ITasklyne.cs
@@ -1,6 +1,14 @@
-﻿namespace Tasklyne.Application;
+﻿using System.Linq.Expressions;
 
-public interface ITasklyne
+namespace Tasklyne.Application.RepositoryInterfaces;
+
+public interface ITasklyne<T> where T : class
 {
+    Task<IEnumerable<T>> GetAllAsync();
+    Task<T> GetByIdAsync(int id);
+    Task<T> GetFirstOrDefaultAsync(Expression<Func<T,bool>> filter, string? includeProperties = null);
+    void Add(T entity);
+    void Remove(T entity);
+    void RemoveRange(IEnumerable<T> entities);
     
 }
